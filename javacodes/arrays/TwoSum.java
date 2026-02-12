095933f14ba72505a7508228da27597a82e7fdd3
Vishal Koli <vishalkoli1402@gmail.com>
2026-02-12 17:07:23 +0530

    TwoSum (leetcode problem no. 1) added in javacodes/arrays

---
+++ javacodes/arrays/TwoSum.java
@@ -0,0 +1,22 @@
+package javacodes.arrays;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[] { 1, 4, 2, 3, 6, 8 }, 4)));
+    }
+}
